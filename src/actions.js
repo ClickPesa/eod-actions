82acbc7f4a101ca57eb748acacc38c4167df79fe
Ben <bmsteve@yahoo.com>
2022-04-24 23:21:58 +0300

    test schedule

--- src/actions.js
+++ src/actions.js
@@ -14,6 +14,7 @@ const { context = {} } = github;
 
 const run = async () => {
   try {
+    console.log(TARGET_BRANCH);
     const pulls = await octokit.request(
       `GET /repos/${context.payload?.repository?.full_name}/pulls`,
       {
